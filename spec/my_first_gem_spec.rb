3b320b652965a6509d3239ed801cca9b483806d6
Leidy Jimenez <leidyjp.001@gmail.com>
2021-03-19 15:51:13 -0500

    specs: Add spec for the method say_hi.

--- spec/my_first_gem_spec.rb
+++ spec/my_first_gem_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe MyFirstGem do
     expect(MyFirstGem::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".say_hi" do
+    it "shows the message hello world" do
+      greeting = described_class.say_hi
+      message = "Hello world"
+      expect(greeting).to eq(message)
+    end
   end
 end
